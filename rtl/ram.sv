b641e277e62a6e1b4df6f256e80f19e4bd2a428e
Emile Badenhorst <myvarhd@gmail.com>
2024-10-08 22:11:33 +0200

    .

--- rtl/ram.sv
+++ rtl/ram.sv
@@ -2,7 +2,9 @@
 
 module ram #(
     ADDR_WIDTH = 31,
-    DATA_WIDTH = 31
+    DATA_WIDTH = 31,
+    NUM_BYTES = 4,
+    BYTE_WIDTH = 8
 )(
     input clk,
     input clk_en,
@@ -12,7 +14,9 @@ module ram #(
     input [ADDR_WIDTH:0] i_read_addr,
     output [DATA_WIDTH:0] o_read_data,
 
-    input [3:0] write_enable,
+   
+    input [3:0] i_write_enable,
+    input [3:0] i_byte_enable,
     input [ADDR_WIDTH:0] i_write_addr,
     input [DATA_WIDTH:0] i_write_data
 );
@@ -28,18 +32,13 @@ module ram #(
 `endif
 
 //2**ADDR_WIDTH - 1 
-(* ram_style = "block" *) reg [DATA_WIDTH:0] mem [2048: 0];
+(* ram_style = "block" *) reg [NUM_BYTES-1:0][BYTE_WIDTH-1:0] mem [2048: 0];
 
 always @(posedge clk) begin
-    if (clk_en) begin
-        if (write_enable[0])
-            mem[i_write_addr][7:0] <= i_write_data[7:0];
-        if (write_enable[1])
-            mem[i_write_addr][15:8] <= i_write_data[15:8];
-        if (write_enable[2])
-            mem[i_write_addr][23:16] <= i_write_data[23:16];
-        if (write_enable[3])
-            mem[i_write_addr][31:24] <= i_write_data[31:24];
+    if (i_write_enable) begin
+        for (int i = 0; i < NUM_BYTES; i = i + 1) begin
+            if(write_enable[i]) mem[i_write_addr][i] <= i_write_data[i*BYTE_WIDTH +: BYTE_WIDTH];
+        end
     end
 end
 
@@ -49,7 +48,5 @@ always @(posedge clk) begin
             o_read_data <= mem[i_read_addr];
     end
 end
-
-
 
 endmodule
