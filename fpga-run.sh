0c82a5cc679acbc6e90f0ad76f88990d6a4fafa6
Emile Badenhorst <myvarhd@gmail.com>
2024-10-19 08:24:15 +0200

    .

--- fpga-run.sh
+++ fpga-run.sh
@@ -1,4 +1,4 @@
 ./build_frimware.sh &
 wait
-python3.13 talk.py -H 10.0.0.105 -p 8880 --retry-interval 5 --minor-pause 0.01 --major-pause 0.05 --write --check Off -f firmware/obj_dir/main.bin --start-address 0x00000000 -o Fatal,Error,Status,Progress
+python3.13 talk.py -H 10.0.0.105 -p 8880 --retry-interval 5 --minor-pause 0.03 --major-pause 0.07 --write --check Off -f firmware/obj_dir/main.bin --start-address 0x00000000 -o Fatal,Error,Status,Progress
 wait
